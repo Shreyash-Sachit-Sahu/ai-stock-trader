8c5e1765871305f9b3803d609fa91331cee96468
Shreyash-Sachit-Sahu <137154025+Shreyash-Sachit-Sahu@users.noreply.github.com>
2024-12-31 02:00:49 +0530

    final

--- frontend/config/apiConfig.ts
+++ frontend/config/apiConfig.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8000' // Replace with your actual API URL
+export const API_BASE_URL = 'http://127.0.0.1:5000' // Replace with your actual API URL
 
 export const API_ENDPOINTS = {
   METADATA: '/',
